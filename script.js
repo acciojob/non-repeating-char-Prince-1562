1fbbb34a686d9c8a6e016753145e4431d13b67cc
Prince-1562 <princemakani1562@gmail.com>
2025-05-12 10:54:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  
+  const frequencyMap = {};
+
+  
+  for (let char of str) {
+    frequencyMap[char] = (frequencyMap[char] || 0) + 1;
+  }
+
+  
+  for (let char of str) {
+    if (frequencyMap[char] === 1) {
+      return char;
+    }
+  }
+
+  
+  return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+
+console.log(firstNonRepeatedChar('aabbcdd'));  
+console.log(firstNonRepeatedChar('aabbcc'));   
+console.log(firstNonRepeatedChar('abc'));      
+console.log(firstNonRepeatedChar(''));         
